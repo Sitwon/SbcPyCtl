4216d466c38689f7856256d180c9bdace4ae1f47
Ben Mendis <dragonwisard@gmail.com>
2011-06-03 15:58:00 -0400

    Some improvements to the convert script

--- convert.py
+++ convert.py
@@ -12,51 +12,49 @@ def convert(line):
 		splitline = line.split(" ", 1)
 	return_type = splitline[0]
 	arg_list = splitline[1].split('(')[1][:-3]
-	arg_dict = {}
+	arg_dict = []
 	for pair in arg_list.split(","):
 		pair = pair.strip()
 		if pair != "":
 			pair = pair.split(" ")
-			arg_dict[pair[0]] = pair[1]
+			arg_dict.append(pair[1])
 	func_name = splitline[1].split()[0].split('(')[0]
-	sys.stdout.write("def " + func_name + "(self")
-	if func_name[:3] == "set":
-		for arg in arg_dict.values():
+	sys.stdout.write("\tdef " + func_name + "(self")
+	if (func_name[:3] == "set") or (func_name[:3] == "add"):
+		for arg in arg_dict:
 			if arg != "pDfvObj":
 				sys.stdout.write(", " + arg)
 	sys.stdout.write("):\n")
 	if return_type == "void":
-		sys.stdout.write("\tdfvifc_c.dfv_" + func_name + "(")
+		sys.stdout.write("\t\tdfvifc_c.dfv_" + func_name + "(")
 		count = 0
-		for arg in arg_dict.values():
-			if count == 0:
-				sys.stdout.write(arg)
-			else:
-				sys.stdout.write(", " + arg)
+		for arg in arg_dict:
+			if count != 0:
+				sys.stdout.write(", ")
+			if arg == "pDfvObj":
+				sys.stdout.write("self.")
+			sys.stdout.write(arg)
 			count += 1
 		sys.stdout.write(")\n")
 	elif return_type == "char*":
-		sys.stdout.write("\tcstr = create_string_buffer(1024)\n")
-		sys.stdout.write("\tdfvifc_c.dfv_" + func_name + ".argtypes = [c_void_p, c_char_p, c_int]\n")
-		sys.stdout.write("\tdfvifc_c.dfv_" + func_name + ".restype = c_char_p\n")
-		sys.stdout.write("\treturn dfvifc_c.dfv_" + func_name + "(")
-		count = 0
-		for arg in arg_dict.values():
-			if count == 0:
-				sys.stdout.write(arg)
-			else:
-				sys.stdout.write(", " + arg)
-			count += 1
-		sys.stdout.write(", cstr, 1024")
+		sys.stdout.write("\t\tcstr = create_string_buffer(1024)\n")
+		sys.stdout.write("\t\tdfvifc_c.dfv_" + func_name + ".argtypes = [c_void_p, c_char_p, c_int]\n")
+		sys.stdout.write("\t\tdfvifc_c.dfv_" + func_name + ".restype = c_char_p\n")
+		sys.stdout.write("\t\treturn dfvifc_c.dfv_" + func_name + "(")
+		sys.stdout.write("self.pDfvObj, cstr, 1024")
 		sys.stdout.write(")\n")
+		if (len(arg_dict) != 3):
+			sys.stderr.write("Warning: " + line)
+			sys.stderr.write("\n")
 	else:
-		sys.stdout.write("\treturn dfvifc_c.dfv_" + func_name + "(")
+		sys.stdout.write("\t\treturn dfvifc_c.dfv_" + func_name + "(")
 		count = 0
-		for arg in arg_dict.values():
-			if count == 0:
-				sys.stdout.write(arg)
-			else:
-				sys.stdout.write(", " + arg)
+		for arg in arg_dict:
+			if count != 0:
+				sys.stdout.write(", ")
+			if arg == "pDfvObj":
+				sys.stdout.write("self.")
+			sys.stdout.write(arg)
 			count += 1
 		sys.stdout.write(")\n")
 	print
