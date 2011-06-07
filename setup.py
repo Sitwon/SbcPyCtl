b1359c6ddeb3b9ee0cbf95ab31066bebe8e82b1c
Ben Mendis <dragonwisard@gmail.com>
2011-06-07 18:52:21 -0400

    Adding a README
    Also added a few more details to setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
 		name="SbcPyCtl",
 		version="0.1dev",
+		author="Ben Mendis",
+		author_email="ben@antennahouse.com",
 		packages=["sbcpyctl",],
+		url="https://github.com/Sitwon/SbcPyCtl",
+		description="Python bindings for Rainbow PDF Server Based Converter.",
 		long_description=open('README').read(),
 	)
 
